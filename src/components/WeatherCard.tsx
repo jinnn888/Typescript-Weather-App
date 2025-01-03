8cc3cdc067aa19b792272c1f0f5ae482207a1246
jinnn888 <tuyor.jynchris@gmail.com>
2025-01-03 14:33:14 +0800

    fetch weather

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,20 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import { optionType } from '../types/index.ts'
-
 import axios from 'axios'
 
-export default function WeatherCard() {
+interface WeatherData {
+  name: string;
+  sys: {
+    country: string
+  };
+  main: {
+    temp: number,
+    humidity: number,
+    feels_like: number,
+    pressure: number,
+    temp_max: number,
+    temp_min: number
+  },
+  weather: [{
+    description: string,
+    icon: string,
+    main: string
+  }],
+  wind: {
+    deg: number,
+    speed: number
+  }
+}
+
+const WeatherCard: React.FC = () => {
   const [location, setLocation] = useState<string>("");
   const [options, setOptions] = useState<[]>([]);
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+
+  const fetchWeatherData = async (lat: number, lon:number) => { 
+    const response = await axios.get<WeatherData>(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_REACT_APP_API_KEY}`
+    );
+    const data = await response.data;
+    return data;
+  }
+
 
   const fetchCoordinates = async (location: string) => {
     const response = await axios.get(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${
-      import.meta.env.VITE_REACT_APP_API_KEY}`
+      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${import.meta.env.VITE_REACT_APP_API_KEY}`
     );
 
     const data = await response.data;
-  
     return data;
   }
 
@@ -33,8 +64,13 @@ export default function WeatherCard() {
       fetch();
   }, [location])
 
-  const optionHandler = async(option: optionType) => {
-    console.log(option)
+  const submitOptionHandler = async(option: optionType) => {
+    const { lat, lon } = option;
+
+    const forecasts = await fetchWeatherData(lat, lon)
+    setOptions([]);
+    setWeatherData(forecasts);
+    console.log(forecasts)
   }
 
     
@@ -48,30 +84,32 @@ export default function WeatherCard() {
             value={location}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => setLocation(e.target.value)}
             placeholder='Search place'/> 
-
-          <button
-            className='text-gray-600 border shadow-sm p-2 rounded text-sm'>
-            Search
-          </button>
         </div>
 
         {/* Options */}
         <div className='absolute p-4'>
           <ul className='text-gray-600 bg-white'>
-            { options && options.map((option: optionType ) => (
-              <li>
+            { options && options.map((option: optionType, index: number) => (
+              <li key={option.name + '-' + index}>
                   <button
-                    onClick={(e) => optionHandler(option)}    
+                    onClick={(e) => submitOptionHandler(option)}    
                     className='hover:bg-zinc-600 hover:text-white text-left text-sm w-full px-2 py-1'>
                     { option.name }
                   </button>
               </li>
             ))}
           </ul>
-
         </div>
 
+        {/* Display Weather Data */}
+        { weatherData && 
+          <div>
+            <h2>{ weatherData.name }</h2>
+          </div>
+        }
       </div>
     </>
   )
 }
+
+export default WeatherCard;
